64b81f968a9360d2873e5728d639d3e2af274685
X <X@X-PC>
2016-09-30 23:24:46 -0400

    final story

--- gitTest/Program.cs
+++ gitTest/Program.cs
@@ -8,6 +8,9 @@ namespace gitTest
     {
         public static void Main(string[] args)
         {
+            bool VA_MIEUX = true;
+            bool NE_VA_PAS_MIEUX = false;
+
             if (string.IsNullOrEmpty(args[0])) return;
             using (var hash = MD5.Create())
             {
@@ -15,8 +18,20 @@ namespace gitTest
                 var myTextHash = BitConverter.ToString(arg0Hash).Replace("-", "");
                 Console.WriteLine(myTextHash);
             }
-            Console.WriteLine($"that it !");
+
             Console.ReadLine();
+            Random santeEmilie = new Random();
+            var etatEmilie = santeEmilie.Next(0, 1) == 1 ? true : false;
+            if (etatEmilie == VA_MIEUX)
+            {
+                Console.WriteLine(@"Émilie va au cims lundi.");
+            }
+            else if (etatEmilie.Equals(NE_VA_PAS_MIEUX))
+            {
+                Console.WriteLine("Les gens du cims seront triste :(.");
+            }
+            Console.ReadLine();
+
         }
     }
 }
