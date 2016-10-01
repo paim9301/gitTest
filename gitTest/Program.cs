e9697f77bfbc41a669e5eca318bcd16c8f32319f
X <X@X-PC>
2016-09-30 22:55:38 -0400

    Add project files.

---
+++ gitTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace gitTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(args[0])) return;
+            using (var hash = MD5.Create())
+            {
+                var arg0Hash = hash.ComputeHash(Encoding.ASCII.GetBytes(args[0]));
+                var myTextHash = BitConverter.ToString(arg0Hash).Replace("-", "");
+                Console.WriteLine(myTextHash);
+            }
+            Console.ReadLine();
+        }
+    }
+}
